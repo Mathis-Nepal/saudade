af2ff69e6a3c7e7c64b4eaba4ed86d8437ece7b4
Nepal Mathis <mathis.nepal@mediaschool.me>
2024-06-12 23:03:06 +0200

    [MIN] preload font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Font } from "../components/components";
 import { Suspense } from "react";
 import Loading from "./loading";
 import { LinkCustom } from "../components/components";
+import config from "../next.config";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,22 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="fr">
             <head>
+            <link
+            rel="preload"
+            href={`${config.basePath}/fonts/Melodrama-Variable.woff2`}
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href={`${config.basePath}/fonts/GarciaMarquez.otf`}
+            as="font"
+            type="font/opentype"
+            crossOrigin="anonymous"
+          />
             <link rel="icon" href={LinkCustom({src : "/assets/favicon.ico"})} />
             </head>
             <Font></Font>
